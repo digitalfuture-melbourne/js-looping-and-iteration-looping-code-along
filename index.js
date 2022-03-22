69ca2eaaa2b6011d7ce3cc15b88ef8da383cb2c3
Issac <info@digitalfuture.melbourne>
2022-03-22 14:17:47 +1100

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(name, text)
+{
+    let i =0
+    let message = []
+    for (i ; i < name.length; i++){
+        message.push(`Thank you, ${name[i]}, for the wonderful ${text} gift!`)
+    }
+    
+    return message
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
